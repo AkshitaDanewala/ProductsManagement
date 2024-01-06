0a4ad709294133231f4f22bd54bc93cf19e22ff2
AkshitaDanewala <akshitadanewala@gmail.com>
2024-01-06 16:06:43 +0530

    created About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
 
 <div className="img h-2/4 w-1/2 bg-[url(https://www.shutterstock.com/image-photo/presentation-multiethnic-office-conference-room-260nw-2101929358.jpg)] rounded bg-center bg-cover bg-no-repeat"></div>
 
-<p className='text-xl w-3/4'>Founded in 1989, ScienceSoft is a provider of IT consulting and software development services. ScienceSoft centers its operations in the US, and delivers services worldwide via offices in the Gulf Cooperation Council and Europe. Throughout the company’s history, we have worked with over 1,200 customers from 75+ countries.</p>
+<p className='text-xl w-3/4'>Founded in 1989, Pinnacle Management is a provider of IT consulting and software development services. Pinnacle Management centers its operations in the US, and delivers services worldwide via offices in the Gulf Cooperation Council and Europe. Throughout the company’s history, we have worked with over 1,200 customers from 75+ countries.</p>
 
 </div>
 
@@ -76,6 +76,187 @@ const About = () => {
 </div>
 
 
+
+<div className="pg3 h-full w-full  border-t px-5 ">
+
+  <h1 className='text-4xl mt-14 text-sky-900 fontfamily'>Solutions We Cover</h1>
+
+  <p className='text-xl w-3/4'>With over 34 years in Information Technology, we have built up expertise in dozens of domains, including:
+</p>
+
+<div className="solutionsbox h-96 w-5/6 shadow shadow-black mt-3 px-12 py-3 ml-20">
+
+<div className="box1 h-28 w-52 border rounded border-solid border-black inline-block   ">
+
+<h1 className='text-xl font-bold text-center mt-5'>Enterprise Applications</h1>
+</div>
+
+<div className="box2 h-28 w-52   border rounded border-solid border-black inline-block ml-6 ">
+
+  <h1 className='text-xl font-bold text-center mt-5'>Supply Chain Management</h1>
+</div>
+
+<div className="box3 h-28 w-52 border rounded border-solid border-black inline-block ml-6">
+    
+    <h1 className='text-xl font-bold text-center mt-5'>Document Management</h1>
+</div>
+
+<div className="box4 h-28 w-52 border rounded border-solid border-black inline-block ml-6">
+
+  <h1 className='text-xl font-bold text-center mt-5'>Operations Management</h1>
+</div>
+
+<div className="box5 h-28 w-52 border rounded border-solid border-black inline-block ">
+
+  <h1 className='text-xl font-bold text-center mt-8'>Corporate Finance</h1>
+</div>
+
+<div className="box6 h-28 w-52 border rounded border-solid border-black inline-block ml-6 mt-2 ">
+
+  <h1 className='text-xl font-bold text-center mt-8'>Payments</h1>
+</div>
+
+<div className="box7 h-28 w-52 border rounded border-solid border-black inline-block ml-6">
+
+  <h1 className='text-xl font-bold text-center mt-8'>Asset Management</h1>
+</div>
+
+<div className="box8 h-28 w-52 border rounded border-solid border-black inline-block ml-6">
+
+  <h1 className='text-xl font-bold text-center mt-8'>Data Analytics</h1>
+</div>
+
+<div className="box9 h-28 w-52 border rounded border-solid border-black inline-block">
+
+  <h1 className='text-xl font-bold text-center mt-8'>Intranets</h1>
+</div>
+
+<div className="box10 h-28 w-52 border rounded border-solid border-black inline-block ml-6">
+
+  <h1 className='text-xl font-bold text-center mt-8'>HR Management</h1>
+</div>
+
+<div className="box11 h-28 w-52 border rounded border-solid border-black inline-block ml-6 mt-2">
+
+  <h1 className='text-xl font-bold text-center mt-8'>eLearning</h1>
+</div>
+
+<div className="box12 h-28 w-52 border rounded border-solid border-black inline-block ml-6">
+
+  <h1 className='text-xl font-bold text-center mt-8'>eCommerce</h1>
+</div>
+
+</div>
+
+
+</div>
+
+
+<div className="team pg3 h-full w-full  border-t  bg-slate-100 px-10 ">
+  <h1 className='text-4xl mt-5 text-sky-900 fontfamily'>Meet the  Pinnacle Management Team</h1>
+
+<div className="memberbox h-96 w-full -black mt-6 flex items-center justify-between px-4">
+
+<div className="member1 h-80 w-64 shadow-lg bg-white rounded-md px-3 py-5 border-t-4 border-sky-800 border-solid">
+
+  <div className="profile h-16 w-16  rounded-full bg-[url(https://www.scnsoft.com/about/rounded/bshyklo.png)] bg-cover bg-center "></div>
+
+  <h2 className='mt-2 text-xl font-semibold'>Brian S. Brooks</h2>
+  <h3>CEO & Founder</h3>
+  <hr className='border-black' />
+
+  <p className='mt-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non consequuntur provident dolores incidunt aliquid iure.</p>
+
+  <div className="icons h-12 w-full  mt-2 flex items-center justify-around">
+
+<h5 className='text-2xl h-11 w-11 bg-sky-800 rounded-full flex items-center justify-center text-white'><i class="ri-instagram-line"></i></h5>
+
+<h5 className='text-2xl  h-11 w-11 bg-sky-800 rounded-full flex items-center justify-center text-white'><i class="ri-facebook-line"></i></h5>
+<h5 className='text-2xl  h-11 w-11 bg-sky-800 rounded-full flex items-center justify-center text-white'><i class="ri-whatsapp-line"></i></h5>
+<h5 className='text-2xl  h-11 w-11 bg-sky-800 rounded-full flex items-center justify-center text-white'><i class="ri-twitter-x-line"></i></h5>
+  </div>
+</div>
+
+
+<div className="member2 h-80 w-64 shadow-lg bg-white rounded-md px-3 py-5 border-t-4 border-sky-800 border-solid">
+
+  <div className="profile h-16 w-16 rounded-full bg-[url(https://www.scnsoft.com/blogauthors/lebedzeva-tatsiana-2.png)] bg-center bg-cover"></div>
+
+  <h2 className='mt-2 text-xl font-semibold'>Kelvin E. Shumate</h2>
+  <h3>Junior Manager</h3>
+  <hr className='border-black' />
+
+  <p className='mt-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non consequuntur provident dolores incidunt aliquid iure.</p>
+
+  <div className="icons h-12 w-full  mt-2 flex items-center justify-around">
+
+<h5 className='text-2xl h-11 w-11 bg-sky-800 rounded-full flex items-center justify-center text-white'><i class="ri-instagram-line"></i></h5>
+
+<h5 className='text-2xl  h-11 w-11 bg-sky-800 rounded-full flex items-center justify-center text-white'><i class="ri-facebook-line"></i></h5>
+<h5 className='text-2xl  h-11 w-11 bg-sky-800 rounded-full flex items-center justify-center text-white'><i class="ri-whatsapp-line"></i></h5>
+<h5 className='text-2xl  h-11 w-11 bg-sky-800 rounded-full flex items-center justify-center text-white'><i class="ri-twitter-x-line"></i></h5>
+  </div>
+</div>
+
+
+<div className="member3 h-80 w-64 shadow-lg bg-white rounded-md px-3 py-5 border-t-4 border-sky-800 border-solid">
+
+  <div className="profile h-16 w-16  rounded-full bg-[url(https://www.scnsoft.com/about/rounded/nikolay-kuraev-2.png)] bg-cover bg-center"></div>
+
+  <h2 className='mt-2 text-xl font-semibold'>Jackie C. Waldman</h2>
+  <h3>Senior Manager</h3>
+  <hr className='border-black' />
+
+  <p className='mt-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non consequuntur provident dolores incidunt aliquid iure.</p>
+
+  <div className="icons h-12 w-full  mt-2 flex items-center justify-around">
+
+<h5 className='text-2xl h-11 w-11 bg-sky-800 rounded-full flex items-center justify-center text-white'><i class="ri-instagram-line"></i></h5>
+
+<h5 className='text-2xl  h-11 w-11 bg-sky-800 rounded-full flex items-center justify-center text-white'><i class="ri-facebook-line"></i></h5>
+<h5 className='text-2xl  h-11 w-11 bg-sky-800 rounded-full flex items-center justify-center text-white'><i class="ri-whatsapp-line"></i></h5>
+<h5 className='text-2xl  h-11 w-11 bg-sky-800 rounded-full flex items-center justify-center text-white'><i class="ri-twitter-x-line"></i></h5>
+  </div>
+</div>
+
+
+<div className="member4 h-80 w-64 shadow-lg bg-white rounded-md px-3 py-5 border-t-4 border-sky-800 border-solid">
+
+  <div className="profile h-16 w-16  rounded-full bg-[url(https://www.scnsoft.com/about/team/viazmin.png)] bg-cover bg-center"></div>
+
+  <h2 className='mt-2 text-xl font-semibold'>Tommy B. Brooks</h2>
+  <h3>Business Consultant</h3>
+  <hr className='border-black' />
+
+  <p className='mt-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non consequuntur provident dolores incidunt aliquid iure.</p>
+
+  <div className="icons h-12 w-full  mt-2 flex items-center justify-around">
+
+<h5 className='text-2xl h-11 w-11 bg-sky-800 rounded-full flex items-center justify-center text-white'><i class="ri-instagram-line"></i></h5>
+
+<h5 className='text-2xl  h-11 w-11 bg-sky-800 rounded-full flex items-center justify-center text-white'><i class="ri-facebook-line"></i></h5>
+<h5 className='text-2xl  h-11 w-11 bg-sky-800 rounded-full flex items-center justify-center text-white'><i class="ri-whatsapp-line"></i></h5>
+<h5 className='text-2xl  h-11 w-11 bg-sky-800 rounded-full flex items-center justify-center text-white'><i class="ri-twitter-x-line"></i></h5>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+</div>
 
 
 
