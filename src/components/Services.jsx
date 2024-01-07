1e7f974c01190a0948f00266f66dd457eba1ceb3
AkshitaDanewala <akshitadanewala@gmail.com>
2024-01-07 10:22:04 +0530

    created services page

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -27,23 +27,182 @@ const Services = () => {
 </div>
 
 
-        
+  <div className="pg2 h-full w-full flex">
+
+    <div className="pg2-left h-full w-1/2 bg-yellow-300 flex items-center justify-center px-4 py-4 bg-[url(https://media.nature.com/lw767/magazine-assets/d41586-018-03071-1/d41586-018-03071-1_15518868.jpg)] bg-cover bg-center rounded-md">
+
+    </div>
+
+    <div className="pg2-right h-full w-1/2 px-3 ">
+
+    <h1 className='text-4xl mt-10 text-sky-900 fontfamily text-center '> Types of Serivices Pinnacle Management Provides </h1>
+
+    <p className='mt-2'>These services play a pivotal role in implementing data governance frameworks, ensuring compliance with regulations, and safeguarding sensitive information from potential threats. By leveraging data management IT services, businesses can streamline their operations, enhance decision-making processes, and stay resilient in the face of evolving technological landscapes.</p>
+
+    <div className="services-box h-64 w-full  flex items-center justify-around mt-6 ">
+
+    <div className="box1 h-44 w-72 border-2 border-solid border-black py-3 rounded px-3">
+
+<h5> <span>◆</span> Software Development</h5>
+<h5> <span>◆</span>  Web Development</h5>
+<h5> <span>◆</span>  Mobile App Development</h5>
+<h5> <span>◆</span>  Testing & QA Development</h5>
+<h5> <span>◆</span>  Application Services Development</h5>
+<h5> <span>◆</span>  UI/UX Design Development</h5>
+
+</div>
+
+    <div className="box2 h-44 w-72 border-2 border-solid border-black py-3 rounded px-3">
+
+<h5> <span>◆</span>  Digital Transformation</h5>
+<h5> <span>◆</span> Managed IT Services</h5>
+<h5> <span>◆</span>  IT Outsourcing</h5>
+<h5> <span>◆</span>  IT Consulting</h5>
+<h5> <span>◆</span>  Data Analytics</h5>
+<h5> <span>◆</span>  Cybersecurity</h5>
+
+    </div>
+
+
+
+
+
+
+
+    </div>
+
+   
+
+
+    </div>
+
+  </div>
+
+
+  <div className="pg3 h-full w-full bg-slate-200 ">
+
+<h1 className='text-4xl  text-sky-900 fontfamily ml-5 py-3'>Tools and Technologies Used for Data Management</h1>
+
+
+<div className="dataIntegrationbox h-36 w-full mt-2">
+<h3 className='text-xl ml-3 font-bold'>Data integration</h3>
+
+<div className="imgesbox h-24 w-full  flex px-4 py-3 justify-around mt-2">
+
+
+<img src="https://www.scnsoft.com/technologies-logos/data-management-services/mssql_server_integration_services_logo.svg" alt="" className='h-16 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/data-management-services/azure-data-factory.svg" alt="" className='h-16 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/big-data/apache-kafka.svg" alt="" className='h-16 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/data-management-services/oracle_data_integrator.svg" alt="" className='h-16 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/data-management-services/talend.svg" alt="" className='h-16 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/data-management-services/azkaban.svg" alt="" className='h-16 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/data-management-services/panoply.svg" alt="" className='h-16 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/data-management-services/informatica.svg" alt="" className='h-16 w-28 bg-white' />
+
+
+</div>
+
+
+
+
+
+
+
+</div>
+
+
+<div className="cloud-databasebox h-36 w-full  mt-2 ">
+<h3 className='text-xl ml-3 font-bold'>Cloud data storage</h3>
+
+<div className="imgesbox h-24 w-full  flex px-4 py-3 justify-around mt-2">
+
+
+<img src="https://www.scnsoft.com/technologies-logos/big-data/azure-cosmos-db-logo.svg" alt="" className='h-18 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/big-data/azure-blob-storage.svg" alt="" className='h-18 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/databases/azure-data-lake-logo.svg" alt="" className='h-18 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/big-data/amazon-dynamodb-logo.svg" alt="" className='h-18 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/big-data/amazon-s3.svg" alt="" className='h-18 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/databases/amazon-rds-logo.svg" alt="" className='h-18 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/databases/mongodb-logo.svg" alt="" className='h-18 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/cloud/amazon-documentdb.svg" alt="" className='h-18 w-28 bg-white' />
+
+
+</div>
+
+
+
+
+
+
+
+</div>
+
+
+<div className="databasebox h-36 w-full  mt-2 ">
+<h3 className='text-xl ml-3 font-bold'>Databases / data storages</h3>
+
+<div className="imgesbox h-24 w-full  flex px-4 py-3 justify-around mt-2">
+
+
+<img src="https://www.scnsoft.com/technologies-logos/databases/ms-sql.svg" alt="" className='h-18 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/databases/mysql.svg" alt="" className='h-18 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/databases/azure-sql-database-logo.svg" alt="" className='h-18 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/databases/oracle.svg" alt="" className='h-18 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/big-data/cassandra.svg" alt="" className='h-18 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/big-data/hive.svg" alt="" className='h-18 w-28 bg-white' />
+
+<img src="https://www.scnsoft.com/technologies-logos/databases/apache-hbase-logo.svg" alt="" className='h-18 w-28 bg-white' />
+
+
+
+</div>
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* pg3end */}
+  </div>
 
        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
